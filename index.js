2b514233e4f28a49aa05fef2a7ff671ab655f5d0
anisShaikh2425 <anisshaikh2555@gmail.com>
2024-02-19 15:06:37 +0530

    blinkit files

---
+++ index.js
@@ -0,0 +1,52 @@
+let shop =document.getElementById("shop")
+
+let shopItemsData=[{
+    id:1,
+    name:"rubon pastueriszed Curd",
+    quantity:1 ,
+    rupees:120
+    
+},
+{
+    id:2,
+    name:"harvest gold white bread",
+    rupees:28
+},
+{
+    id:3,
+    name:"Mother dairy classic",
+    rupees:34
+},
+{
+    id:4,
+    name:"Amul Salted Butter",
+    rupees:33
+}]
+
+let generateShop=()=>{
+    shopItemsData.map((x)=>{
+        return 
+    })
+}
+
+
+
+// // document.querySelector("cartBtn").addEventListener("click" , opencCart);
+// // function opencCart(){
+// //     document.querySelector("showCart").style.visibility="visible";
+// // }
+
+//  document.querySelector(".cartBtn").addEventListener("click" ,function(){
+//    document.querySelector(".showCart").style.visibility="visible";
+//  });
+
+// document.querySelector(".crossbtn").addEventListener("click", function(){
+//     document.querySelector(".showCart").style.visibility="hidden";
+// })
+
+// document.querySelectorAll("btn").addEventListener("click",function(){
+//   document.querySelector("cartBtn").innerHTML="150";
+// } )
+// //  function close(){
+// //     document.querySelector(".showCart").style.visibility="hidden";
+// //  }f
